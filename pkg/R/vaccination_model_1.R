f747f43720ea0ba3b8fc66dfcbfb7f53b0a0c7e9
James T. Murphy <jtm6@uw.edu>
2020-01-14 10:57:46 -0800

    additional comments to "vaccine model 1" file

--- pkg/R/vaccination_model_1.R
+++ pkg/R/vaccination_model_1.R
@@ -13,6 +13,8 @@ phi1 <- function(dat, at) {
   # NA to "1" to eligibe/selected agents.
   #2) After vacc. campaign has started, will change status of vaccinated agent's whose 
   #efficacy has waned to "0" (stochastic draw)
+  #3)this function is put in the "module" list in run script (called each timestep)
+  
   
   
   #skip vaccination routine if vacc. campaign hasn't started
@@ -30,8 +32,7 @@ phi1 <- function(dat, at) {
   if(!is.element(at,dat$param$start_vacc_campaign)) {return(dat)}
   #------------------------------------------------
   
-  # If vaccine is targeted to attribute groups
-  
+
   #if designated vacc. level reached (percent of pop vaccianted), don't vacc anymore
   if(length(which(dat$pop$phi == 1 ))/length(which(dat$pop$Status>=0)) > dat$param$max_perc_vaccinated){return(dat)}
   
@@ -43,7 +44,7 @@ phi1 <- function(dat, at) {
   
   if(length(eligible_index) == 0) {return(dat)}  #if no agents are eligible
   
-  no_vaccinated <- sum(rbinom(length(which(dat$pop$Status>=0)), 1, dat$param$perc_vaccinated)) #denominator is total population alive 
+  no_vaccinated <- sum(rbinom(length(which(dat$pop$Status>=0)), 1, dat$param$perc_vaccinated_rate)) #denominator is total population alive 
   if(no_vaccinated == 0) {return(dat)}
   
   
@@ -64,6 +65,8 @@ phi1 <- function(dat, at) {
 #' @export
 update_mu1 <- function (dat,at){
   #code to initialize/update  mu,sigma for each agent or initialize for new agents to model
+  #this function is put in the "module" list in run script (called each timestep)
+  
   # for model 1, this fxn just returns dat object without modification (no mu,sigma to update)
   
   #note: "m" and "sigma" are agent attributes (see file/fxn "input_parameters_agent_attributes)
@@ -75,9 +78,12 @@ update_mu1 <- function (dat,at){
 
 #' @export
 covariates1 <- function(dat,at){
-  #initializupdate baseline covariates necessary for vaccine model's "calculate_theta" fxn
-  #that are not already part of baseline evonete/
+  #initializ and update baseline covariates necessary for vaccine model's "calculate_theta" fxn
+  #that are not already part of baseline evonet
+  #this function is put in the "module" list in run script (called each timestep)
+  
   #for model 1, no covariates so just return "dat$covariates" object unmodificed
+  
   if(at==2){
     dat$covariates <- NA
   }
@@ -87,45 +93,57 @@ covariates1 <- function(dat,at){
 ######################
 
 #' @export
-marks1 <- function(dat,at){
-  #at start of model, initialize "marks" for each agent
-  #after start, for given agent specific mu/sigma in dat$mu and dat$sigma,
-  #calculate updated marks,stored in dat$m
-  
-  #for model1, marks for each agent is static, that is, agent is either infected or not
-  #thus, for model1, marks is just the "Status" attribute (0/1)
-  
-  dat$pop$m <- dat$pop$Status
-  return(dat)
+marks1 <- function(dat,index,at){
+#for initially infected (model start, at=1), binomial draw to determine whether virus is "sensitive" to vaccine or not
+# called in "initialize_infecteds_vl", line 129
+
+#for secondary infections, called in "transmission_bookkeeping_module" , line 206
+#for model 1, simply returns marks of infector
+
+  #note at=1 is model initialization
+if(at==1){
+  rbinom(length(index),1,
+         prob=dat$param$perc_virus_vaccine_sens)
+  #note: param$perc_virus_vaccine_sens is a user-specified param (default: 0.99)
+}else{    
+  dat$pop$m[index]
+}
   
 }
 ######################
 #' @export
-draw_m1 <- function(dat,at){
+draw_m1 <- function(dat){
   # calculate marks, based on  most current values of mu and sigma 
+  # called in "calculate_theta1", see below
   
   #for model 1, marks don't change, it is whether infector's virus is sensitive
   #to infectee's vaccine (0/1)
-  m <- dat$pop$virus_sens_vacc[dat$infector_id]
+  
+  m <- dat$pop$m[dat$infector_id]
   return(m)
 }
 #######################
 
 #' @export
-caclulate_theta1 <- function(dat){
+calculate_theta1 <- function(dat){
   # draw updated marks (of virus)
   # then caclulate theta based on dat$m, dat$phi, dat$covariates
   # output theta to adjust raw trans probs
+  #called at "transmission_main_module" line 157
   
   #for model 1, mark is whether virus of infector is sensitive to vaccine of
   # infectee
 
-  m <- draw_m1(dat,at)
+  m <- draw_m1(dat)
   #theta is  vaccination status (0/1) * 
   #             virus susceptibility to vaccine (0/1) *
   #                percent reduction due to vaccine in trans. prob.
   
-  theta <- dat$pop$phi[dat$susceptible_id]*m*dat$param$vacc_trans_prob_decrease
+  theta <- rep(1,length(dat$susceptible_id))
+  index <- which(dat$pop$phi[dat$susceptible_id]==1)
+  if(length(index)==1){
+    theta[index]  <-(dat$pop$phi[dat$susceptible_id])[index]*m[index]*dat$param$vacc_trans_prob_decrease
+  }
   return(theta)
 }
 
